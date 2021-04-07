2facaf4629ee6f0696ced6099727a4434227c9aa
Justiiice <32276752+Justiiice@users.noreply.github.com>
2021-04-07 16:36:57 +0200

    a lot of changes. New font. Button says start, then continue, then reset. Poem print and tree append are their own functions. Ttree now changes color. Added comments++

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,47 +9,68 @@ resetPage();
 
 continueButton.addEventListener('click', (e) => {
     pageNumber++;
-    if (pageNumber != 4) {
-        gradPos -= 25;
-    }
-
-    document.getElementById("poem-text").innerHTML = ``;
-
-    if (pageNumber <= 5) {
-        updatePage();
-    }
-    if (pageNumber > 5) {
-        resetPage();
-    }
+    continueButton.style.opacity = '0';
+    continueButton.innerHTML = 'Continue'
+    if (pageNumber != 4) { gradPos -= 20; }
+    if (pageNumber <= 6) { updatePage(); }
+    if (pageNumber === 7) { resetPage(); }
 });
+
+function printPoem() {
+    // prints all lines of current paragraph of the poem matching the page number.
+    poemText.innerHTML = '';
+    poem["poem-" + pageNumber].forEach((line, i) => {
+        const paragraph = document.createElement("P");
+        paragraph.innerHTML = line;
+        paragraph.className = "para";
+        setTimeout(function () { paragraph.className = 'para fadeIn'; }, i * 1200 + 10);
+        poemText.appendChild(paragraph);
+    });
+}
 
 function updatePage() {
     body.className = `page-${pageNumber}`;
-    poem["poem-" + pageNumber].forEach(line => {
-        const paragraph = document.createElement("P");
-        paragraph.innerHTML = line;
-        poemText.appendChild(paragraph);
-    });
+    setTimeout(function () {
+        continueButton.style.opacity = '100'
+    }, 4500);
+
+    // changes the position of the background gradient. 
     body.style.backgroundPosition = gradPos + "%";
+
     switch (pageNumber) {
         case 0:
+            printPoem()
             break;
         case 1:
+            printPoem()
+            makeTree('green');
+            console.log('yeehaw')
             break;
         case 2:
-            poem2();
+            printPoem()
+            removeTree()
+            makeTree('red');
             break;
         case 3:
+            removeTree()
+            makeTree('white');
             break;
         case 4:
+            printPoem()
+            removeTree()
             break;
         case 5:
+            printPoem()
+            break;
+        case 6:
+            printPoem()
             continueButton.innerHTML = "Reset";
             break;
     }
 }
 
 function resetPage() {
+    poemText.innerHTML = '';
     pageNumber = 0;
     gradPos = 100;
     body.style.backgroundPosition = gradPos + "%";
@@ -59,39 +80,70 @@ function resetPage() {
     const paragraph = document.createElement("P");
     paragraph.innerHTML = "by Joni Mitchell";
     poemText.appendChild(paragraph);
-    continueButton.innerHTML = "Continue";
+    continueButton.innerHTML = "Start";
     body.className = `page-0`;
+    continueButton.style.opacity = '100';
 }
 
-function poem2() {
+function removeTree(time = 2200, num = 0) {
+    // removes the tree object from the document.
+    // might animate this instead hmmmmm
+    let trees = document.getElementsByClassName("tree");
+    if (!trees) return
+    trees[num].className = 'tree fadeOut';
+    console.log(trees[num])
+    setTimeout(function () {
+        trees[num].remove()
+    }, time);
+}
+function makeTree(leafColor) {
     const tree = document.createElement("DIV");
     tree.className = "tree";
+    setTimeout(function () {
+        tree.className = 'tree fadeIn';
+        console.log('this is dumb but it does work')
+    }, 10);
     const topTree = document.createElement("DIV");
     topTree.className = "topTree";
+
+    // making leaves for the tree.
     for (i = 0; i < 30; i++) {
         const leaves = document.createElement("DIV");
         leaves.className = "leaves";
+
+        // size will be randomly 5, 10 or 15
         const randomSize = getRandomInt(3);
         let size = 5;
-        if (randomSize === 0) {
-            size = 5;
-        } else if (randomSize === 1) {
-            size = 10;
-        } else if (randomSize === 2) {
-            size = 15;
-        }
+        if (randomSize === 1) { size = 10; }
+        if (randomSize === 2) { size = 15; }
+
+        // color will be randomly three similar shades of color from param
+        let color = "rgba(255, 255, 255, 0.5)";
         const randomColor = getRandomInt(3);
-        let color = "rgba(198, 204, 109, 0.788)";
-        if (randomColor === 0) {
-            color = "rgba(198, 204, 109, 0.788)";
-        } else if (randomColor === 1) {
-            color = "rgba(151, 185, 112, 0.76)";
-        } else if (randomColor === 2) {
-            color = "rgba(51, 83, 44, 0.76)";
+        switch (leafColor) {
+            case 'green':
+                if (randomColor === 0) { color = "rgba(85, 255, 85, 0.788)"; }
+                if (randomColor === 1) { color = "rgba(98, 204, 71, 0.76)"; }
+                if (randomColor === 2) { color = "rgba(21, 65, 11, 0.76)"; }
+                break;
+            case 'red':
+                if (randomColor === 0) { color = "rgb(236, 136, 22, 0.76)"; }
+                if (randomColor === 1) { color = "rgb(236, 90, 22, 0.76)"; }
+                if (randomColor === 2) { color = "rgb(241, 126, 31, 0.76)"; }
+                break;
+            case 'white':
+                if (randomColor === 0) { color = "rgb(255, 255, 255, 0.46)"; }
+                if (randomColor === 1) { color = "rgb(255, 255, 255, 0.56)"; }
+                if (randomColor === 2) { color = "rgb(255, 255, 255, 0.66)"; }
+                break;
         }
+
+        // position of each leaf will be randomly placed within the top 70% of the tree and randomly 50% from either left or right.
         const bottom = getRandomInt(70);
         const leftOrRight = getRandomInt(2) === 1 ? "left" : "right";
         const horizontal = getRandomInt(50);
+
+        // adding all variables to each leaf's individual style.
         leaves.style = `
             height: ${size}vmin;
             width: ${size}vmin;
@@ -101,13 +153,29 @@ function poem2() {
         `;
         topTree.appendChild(leaves);
     }
+
+    // if (leafColor === 'red') {
+    //     setTimeout(function () {
+    //         fadeToWhite()
+    //     }, 2000);
+    // }
+    // function fadeToWhite() {
+    //     removeTree()
+    //     makeTree('white')
+    // }
+
+    // adding a tree trunk
     const trunk = document.createElement("DIV");
     trunk.className = "trunk";
+
+    // adding 3 branches to the trunk.
     for (i = 0; i < 3; i++) {
         const branch = document.createElement("DIV");
         branch.className = "branch";
         trunk.appendChild(branch);
     }
+
+    // adding the leaves and the trunk to the tree and appending the tree to the body. (css styles it more)
     tree.appendChild(topTree);
     tree.appendChild(trunk);
     body.appendChild(tree);
@@ -166,18 +234,21 @@ const poem = {
         'when all their leaves are shed."'
     ],
     "poem-3": [
+        'this wont print, kinda cheeky solution i guess'
+    ],
+    "poem-4": [
         '"I know the sky is blue," she said.',
         '"And silver is the sand;',
         'And apples are the brightest red.',
         'What colour is a man?"'
     ],
-    "poem-4": [
+    "poem-5": [
         '"Man is many colours child:',
         'Some are yellow, some are brown.',
         'And some are black as sightlessness,',
         'Some white as eiderdown."'
     ],
-    "poem-5": [
+    "poem-6": [
         'She took her crayons from a box',
         'And placed them in my glove',
         'And said, "By mixing all of these',
